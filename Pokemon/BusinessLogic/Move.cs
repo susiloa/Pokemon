8e74091db766049b9e44d52d732a368a2287dfba
avancenm <avancenm@seridancollege.ca>
2020-03-31 22:04:48 -0400

    Initialized Moce class

--- Pokemon/BusinessLogic/Move.cs
+++ Pokemon/BusinessLogic/Move.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.Storage;
+
+
+//Sofia Avancena
 
 namespace Pokemon.BusinessLogic
 {
     class Move
     {
+		/* Read only properties */
+		private string _moveName;
+        public string MoveName
+		{
+			get { return _moveName; }
+		}
+        private string _moveType;
+        public string MoveType
+        {
+            get { return _moveType; }
+        }
+        private int _moveAccuracy;
+        public int MoveAccuracy
+        {
+            get { return _moveAccuracy; }
+        }
+        private int _movePower;
+        public int MovePower
+        {
+            get { return _movePower; }
+        }
+
+        public Move(string name, string type, int accuracy, int power)
+        {
+            this.MoveName = name;
+            this.MoveType = type;
+            this.MoveAccuracy = accuracy;
+            this.MovePower = power;
+        }
+
     }
 }
