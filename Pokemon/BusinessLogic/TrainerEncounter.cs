e767c3e5957508accf81bc95b30358a919249795
avancenm <avancenm@seridancollege.ca>
2020-04-18 19:45:07 -0400

    Edited TrainerEncounter.cs

--- Pokemon/BusinessLogic/TrainerEncounter.cs
+++ Pokemon/BusinessLogic/TrainerEncounter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Automation;
 
 //Sofia Avanccena
 
@@ -14,22 +15,43 @@ namespace Pokemon.BusinessLogic
     the next pokemon in the list would be moved to the current active pokemon  */
     class TrainerEncounter: Encounter
     {
-        public static List<Pokemon> GetPokemonSet()
+        //Properties
+        public Pokemon ActivePokemon { get; set; }
+        public List<Pokemon> _reservePokemon { get; set; }
+        private string _reward;
+
+        public string Reward
         {
-            List<Pokemon> pokemonSet = new List<Pokemon>();
-            pokemonSet.Add(new Pokemon
-            {
+            get { return _reward}
+        }
 
-            });
-            pokemonSet.Add(new Pokemon
-            {
+        //Constructor
+        public TrainerEncounter(Pokemon pokemon, Encounter reward)
+        {
 
-            });
-            pokemonSet.Add(new Pokemon
-            {
+        }
 
-            });
-            return pokemonSet;
+        //Methods
+        public override bool BattleLost()
+        {
+            if (ActivePokemon.Health == 0 && _reservePokemon.Count == 0) return true;
+            else if (_reservePokemon.Count >= 1)
+            {
+                NextPokemon();
+                return false;
+            }
+            else return false;
+        }
+
+        public void NextPokemon()
+        {
+            ActivePokemon = _reservePokemon[0];
+            _reservePokemon.RemoveAt(0);
+        }
+
+        public override string GetReward()
+        {
+            return "You won a badge and 100 EXP! Congratulations, you are one step closer to becoming a pokemon master!";
         }
     }
 }
