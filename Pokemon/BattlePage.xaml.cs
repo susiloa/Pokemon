aa8cf1143ede2f4238c86b6e522f24ef4a7b3e96
Oliver Drenth <oliverdrenth@gmail.com>
2020-04-18 21:19:39 -0400

    home button in battlepage as well as battlepage.xaml.cs framework created

--- Pokemon/BattlePage.xaml.cs
+++ Pokemon/BattlePage.xaml.cs
@@ -27,9 +27,35 @@ namespace Pokemon
             this.InitializeComponent();
         }
 
+        private void Move1_OnClickBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ***Run the damage formula on the enemy***
+        }
+
+        private void Move2_OnClickBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ***Run the damage formula on the enemy***
+        }
+
+        private void Move3_OnClickBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ***Run the damage formula on the enemy***
+        }
+
         private void HomeBtn_OnClickBtn_OnClick(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage), null);
         }
+
+        PlayerPokemonImg.Source = new BitmapImage(new Uri(uriString: $"ms-appx:///Assets/{*** PLAYER POKEMON NAME FROM JSON FILE***}.png", UriKind.RelativeOrAbsolute));
+        EnemyPokemonImg.Source = new BitmapImage(new Uri(uriString: $"ms-appx:///Assets/{***ENEMY POKEMON NAME FROM JSON FILE***}.png", UriKind.RelativeOrAbsolute));
+        PlayerPokemonHealth.Text = ***PLAYERS POKEMON HEALTH FROM JSON FILE***;
+        EnemyPokemonHealth.Text = ***ENEMY POKEMON HEALTH FROM JSON FILE***;
+        Move1.Text = ***THE FIRST MOVE FROM THE JSON FILE***;
+        Move2.Text = ***THE SECOND MOVE FROM THE JSON FILE***;
+        Move3.Text = ***THE THIRD MOVE FROM THE JSON FILE***;
+
+
+
     }
 }
